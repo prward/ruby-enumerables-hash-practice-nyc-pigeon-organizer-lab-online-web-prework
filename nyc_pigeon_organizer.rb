2102ad5f978f2f8326f9984ae1ad8ee42a969af0
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-26 14:51:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,5 @@ def nyc_pigeon_organizer(data)
   data.each do |color_gender_lives, value|
     value.each do |stats, all_names|
       all_names.each do |name|
-        
+        pigeon_list[:name] ||= {}
 end
